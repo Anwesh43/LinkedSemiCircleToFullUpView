c33875c26bd7a3d857df5b7759e43a7c03946872
Anwesh43 <anweshthecool0@gmail.com>
2021-06-30 02:26:12 +0530

    adding SCTFUNode class

--- semicircletofullview/src/main/java/com/example/semicircletofullview/SemiCircleToFullUpView.kt
+++ semicircletofullview/src/main/java/com/example/semicircletofullview/SemiCircleToFullUpView.kt
@@ -116,4 +116,45 @@ class SemiCircleToFullUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SCTFUNode(var i : Int, val state : State = State()) {
+
+        private var next : SCTFUNode? = null
+        private var prev : SCTFUNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SCTFUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSCTFUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SCTFUNode {
+            var curr : SCTFUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
