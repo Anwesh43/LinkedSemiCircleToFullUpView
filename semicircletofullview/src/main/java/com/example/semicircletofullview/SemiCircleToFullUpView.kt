c4baf98f47f3b7a2fc3f380590d5bdcaff9d2615
Anwesh43 <anweshthecool0@gmail.com>
2021-06-30 01:51:49 +0530

    adding constants and libraries

---
+++ semicircletofullview/src/main/java/com/example/semicircletofullview/SemiCircleToFullUpView.kt
@@ -0,0 +1,28 @@
+package com.example.semicircletofullview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#004D40",
+    "#FFD600",
+    "#00C853",
+    "#6200EA"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 180f
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+
