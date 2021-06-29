323dcd435558f26b9415175dc7eda70fffb0c591
Anwesh43 <anweshthecool0@gmail.com>
2021-06-30 02:42:10 +0530

    adding renderer in view

--- semicircletofullview/src/main/java/com/example/semicircletofullview/SemiCircleToFullUpView.kt
+++ semicircletofullview/src/main/java/com/example/semicircletofullview/SemiCircleToFullUpView.kt
@@ -57,14 +57,16 @@ fun Canvas.drawSCTFUNode(i : Int, scale : Float, paint : Paint) {
 
 class SemiCircleToFullUpView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
