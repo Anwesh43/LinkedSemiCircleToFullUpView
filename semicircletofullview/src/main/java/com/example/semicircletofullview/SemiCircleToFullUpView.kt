0cb62ab7a5825e1e14e107692c317ee2e2cc9217
Anwesh43 <anweshthecool0@gmail.com>
2021-06-30 02:09:15 +0530

    adding Canvas extension methods

--- semicircletofullview/src/main/java/com/example/semicircletofullview/SemiCircleToFullUpView.kt
+++ semicircletofullview/src/main/java/com/example/semicircletofullview/SemiCircleToFullUpView.kt
@@ -29,3 +29,28 @@ val scGap : Float = 0.02f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSemiCircleToFull(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2 + (w / 2 + size) * sc3,  h + size / 2 - (h / 2) * sc1)
+    for (j in 0..1) {
+        save()
+        rotate(rot * j * sc2)
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, rot, true, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSCTFUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSemiCircleToFull(scale, w, h, paint)
+}
