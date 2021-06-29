76969f5bfd77380a3000e86a0d699e5348cebc81
Anwesh43 <anweshthecool0@gmail.com>
2021-06-30 02:12:35 +0530

    adding SemiCircleToFullView class

--- semicircletofullview/src/main/java/com/example/semicircletofullview/SemiCircleToFullUpView.kt
+++ semicircletofullview/src/main/java/com/example/semicircletofullview/SemiCircleToFullUpView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawSCTFUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSemiCircleToFull(scale, w, h, paint)
 }
+
+class SemiCircleToFullUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
